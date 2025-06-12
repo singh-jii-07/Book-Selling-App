e1ad093521f7b758a46ea2e436e7be4e794a1280
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-06-12 11:31:28 -0500

    All favourite routs is done

--- Backend/App/Routes/Favourite.js
+++ Backend/App/Routes/Favourite.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { authenticateToken } from '../Middleware/Useraauthor.js';
-import { addFavourite, deletFavourite } from '../controllers/Favourite.js';
+import { addFavourite, deletFavourite, getFavourite } from '../controllers/Favourite.js';
 
 const favouriteRoutes = express.Router();
 favouriteRoutes.put('/addFavourite',authenticateToken,addFavourite)
 favouriteRoutes.delete('/deletFavourite',authenticateToken,deletFavourite)
+favouriteRoutes.get('/getfavourite',authenticateToken,getFavourite)
 
 export{favouriteRoutes}
--- Backend/App/controllers/Favourite.js
+++ Backend/App/controllers/Favourite.js
@@ -1,4 +1,4 @@
-import User from "../modules/User.js"; 
+import User from "../modules/User.js";  
 let  addFavourite=async(req,res)=>{
     const {bookid,id}=req.headers;
     try {
@@ -36,4 +36,29 @@ let  deletFavourite=async(req,res)=>{
     }
 }
 
-export{addFavourite,deletFavourite}
+const getFavourite = async (req, res) => {
+    try {
+        const userId = req.headers.id;
+
+        if (!userId) {
+            return res.status(400).json({ message: "User ID not provided in headers" });
+        }
+
+        const userData = await User.findById(userId)
+          
+
+        if (!userData) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.status(200).json({ message: "Success", data: userData.favourites });
+    } catch (err) {
+        console.error("Internal Server Error:", err);
+        res.status(500).json({ message: "Error occurred", error: err.message || err });
+    }
+};
+
+
+
+
+export{addFavourite,deletFavourite,getFavourite}
