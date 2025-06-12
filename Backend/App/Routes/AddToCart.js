184cdacf29e272e323bc979ea609477310a75859
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-06-12 12:04:56 -0500

    add to cart  change

--- Backend/App/Routes/AddToCart.js
+++ Backend/App/Routes/AddToCart.js
@@ -3,6 +3,6 @@ import { authenticateToken } from '../Middleware/Useraauthor.js';
 import { addToCart,deleteToCart,getToCart } from '../controllers/AddToCart.js';
 const cartRoutes = express.Router();
 cartRoutes.put('/addCart',authenticateToken,addToCart)
-cartRoutes.put('/deleteCart',authenticateToken,deleteToCart)
+cartRoutes.put('/deleteCart/:bookid',authenticateToken,deleteToCart)
 cartRoutes.get('/getCart',authenticateToken,getToCart)
 export{cartRoutes}
